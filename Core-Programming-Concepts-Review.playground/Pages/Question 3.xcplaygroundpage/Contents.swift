df8e680091ed9ce20b42f00e485299dcb615bfa5
Zhang Matt <mzhang@lcs.on.ca>
2018-01-21 15:24:31 -0500

    Did another question

--- Core-Programming-Concepts-Review.playground/Pages/Question 3.xcplaygroundpage/Contents.swift
+++ Core-Programming-Concepts-Review.playground/Pages/Question 3.xcplaygroundpage/Contents.swift
@@ -15,6 +15,15 @@
  */
 // Answer question 3 below
 
+var piggyBank = 0
+
+piggyBank += 10
+piggyBank += 20
+piggyBank /= 2
+piggyBank *= 3
+piggyBank -= 3
+
+
 /*:
  ## Now share your understanding
  
